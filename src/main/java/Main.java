480769b903d63c1a2f18ac1aa5da0b1418be4cfc
tarGz <ocittwo@gmail.com>
2016-12-17 23:44:46 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ocittwo on 12/17/16.
+ *
+ * @Author Ahmad Rosid
+ * @Email ocittwo@gmail.com
+ * @Github https://github.com/ar-android
+ * @Web http://ahmadrosid.com
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+
+}
